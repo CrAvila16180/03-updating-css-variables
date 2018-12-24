58ac293d81bcf20e75a271e89e235257f0739bee
CR_Avila <carlosrobertoavilah@gmail.com>
2018-12-24 11:14:46 -0600

    Handle updates

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const inputs = document.querySelectorAll('.controls input');
 
-let handleUpdate = () => {
-    console.log(this.value);
+function handleUpdate() {
+    
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
